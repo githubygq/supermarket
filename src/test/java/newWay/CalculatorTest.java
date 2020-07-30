e22d82ae5232efcbdb1417d75db4418a8e215be8
ygq <493650029@qq.com>
2020-07-30 10:24:08 +0800

    需求一should_return_110count_when_calculate_given_goods_price_110

--- src/test/java/newWay/CalculatorTest.java
+++ src/test/java/newWay/CalculatorTest.java
@@ -9,5 +9,17 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class CalculatorTest {
-
+    @Test
+    public void should_return_110count_when_calculate_given_goods_price_110(){
+        //given
+        List<ChargeWay> chargeWays = Arrays.asList(new NormalChargeWay());
+        Goods washingliquid = new Goods( 110,GoodsType.WASHINGLIQUID);
+        List<Goods> goods = new ArrayList<>();
+        goods.add(washingliquid);
+        Calculator calculator = new Calculator();
+        //when
+        int count = calculator.calculate(goods,chargeWays);
+        //then
+        assertThat(count, is(110));
+    }
 }
